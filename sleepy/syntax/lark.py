1bab4c58a8909a116d0d93acc592259f28be8b0b
Victor Smirnov <vityaman.dev@yandex.ru>
2023-12-11 16:58:01 +0300

    #5 [syntax] added is_same_as to compare trees

--- sleepy/syntax/lark.py
+++ sleepy/syntax/lark.py
@@ -16,6 +16,9 @@ class _SyntaxTree(ABC, Ast):
     def pythonic(self) -> PythonRepr:
         raise NotImplementedError
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        raise NotImplementedError
+
 
 class _Expression(_SyntaxTree):
     pass
@@ -33,6 +36,16 @@ class Program(_SyntaxTree, AsList, WithMeta):
     def pythonic(self) -> PythonRepr:
         return tuple(e.pythonic() for e in self.expressions)
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Program) and all(
+            this.is_same_as(that)
+            for this, that in zip(
+                self.expressions,
+                other.expressions,
+                strict=True,
+            )
+        )
+
 
 @dataclass
 class Symbol(_Atomic, WithMeta):
@@ -41,6 +54,9 @@ class Symbol(_Atomic, WithMeta):
 
     def pythonic(self) -> PythonRepr:
         return self.name
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Symbol) and self.name == other.name
 
 
 @dataclass
@@ -51,6 +67,11 @@ class _Integer(_Atomic, WithMeta):
     def pythonic(self) -> PythonRepr:
         return self.value
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, _Integer) and self.value == other.value
+        )
+
 
 @dataclass
 class String(_Atomic, WithMeta):
@@ -60,6 +81,9 @@ class String(_Atomic, WithMeta):
     def pythonic(self) -> PythonRepr:
         return self.value
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, String) and self.value == other.value
+
 
 @dataclass
 class Kind(_SyntaxTree, WithMeta):
@@ -68,6 +92,11 @@ class Kind(_SyntaxTree, WithMeta):
 
     def pythonic(self) -> PythonRepr:
         return self.name.pythonic()
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Kind) and self.name.is_same_as(
+            other.name,
+        )
 
 
 @dataclass
@@ -79,6 +108,13 @@ class Parameter(_SyntaxTree, WithMeta):
     def pythonic(self) -> PythonRepr:
         return (self.kind.pythonic(), self.kind.pythonic())
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, Parameter)
+            and self.symbol.is_same_as(other.symbol)
+            and self.kind.is_same_as(other.kind)
+        )
+
 
 @dataclass
 class Parameters(_SyntaxTree, AsList, WithMeta):
@@ -88,6 +124,16 @@ class Parameters(_SyntaxTree, AsList, WithMeta):
     def pythonic(self) -> tuple:
         return tuple(e.pythonic() for e in self.parameters)
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Parameters) and all(
+            this.is_same_as(that)
+            for this, that in zip(
+                self.parameters,
+                other.parameters,
+                strict=True,
+            )
+        )
+
 
 @dataclass
 class Body(_SyntaxTree, AsList, WithMeta):
@@ -96,6 +142,16 @@ class Body(_SyntaxTree, AsList, WithMeta):
 
     def pythonic(self) -> tuple:
         return tuple(e.pythonic() for e in self.expressions)
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Body) and all(
+            this.is_same_as(that)
+            for this, that in zip(
+                self.expressions,
+                other.expressions,
+                strict=True,
+            )
+        )
 
 
 @dataclass
@@ -111,6 +167,13 @@ class Lambda(_Expression, WithMeta):
             *self.body.pythonic(),
         )
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, Lambda)
+            and self.parameters.is_same_as(other.parameters)
+            and self.body.is_same_as(other.body)
+        )
+
 
 @dataclass
 class Condition(_SyntaxTree, WithMeta):
@@ -119,6 +182,11 @@ class Condition(_SyntaxTree, WithMeta):
 
     def pythonic(self) -> PythonRepr:
         return self.expression.pythonic()
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(
+            other, Condition
+        ) and self.expression.is_same_as(other.expression)
 
 
 @dataclass
@@ -129,6 +197,12 @@ class ThenBranch(_SyntaxTree, WithMeta):
     def pythonic(self) -> PythonRepr:
         return self.expression.pythonic()
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(
+            other,
+            ThenBranch,
+        ) and self.expression.is_same_as(other.expression)
+
 
 @dataclass
 class ElseBranch(_SyntaxTree, WithMeta):
@@ -137,6 +211,12 @@ class ElseBranch(_SyntaxTree, WithMeta):
 
     def pythonic(self) -> PythonRepr:
         return self.expression.pythonic()
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(
+            other,
+            ElseBranch,
+        ) and self.expression.is_same_as(other.expression)
 
 
 @dataclass
@@ -154,6 +234,14 @@ class IfExpression(_Expression, WithMeta):
             self.else_branch.pythonic(),
         )
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, IfExpression)
+            and self.condition.is_same_as(other.condition)
+            and self.then_branch.is_same_as(other.then_branch)
+            and self.else_branch.is_same_as(other.else_branch)
+        )
+
 
 @dataclass
 class VariableDefinition(_Expression, WithMeta):
@@ -168,6 +256,13 @@ class VariableDefinition(_Expression, WithMeta):
             self.expression.pythonic(),
         )
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, VariableDefinition)
+            and self.symbol.is_same_as(other.symbol)
+            and self.expression.is_same_as(other.expression)
+        )
+
 
 @dataclass
 class Invokable(_SyntaxTree, WithMeta):
@@ -176,6 +271,12 @@ class Invokable(_SyntaxTree, WithMeta):
 
     def pythonic(self) -> PythonRepr:
         return self.expression.pythonic()
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(
+            other,
+            Invokable,
+        ) and self.expression.is_same_as(other.expression)
 
 
 @dataclass
@@ -186,6 +287,16 @@ class Args(_SyntaxTree, AsList, WithMeta):
     def pythonic(self) -> tuple:
         return tuple(e.pythonic() for e in self.expressions)
 
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return isinstance(other, Args) and all(
+            this.is_same_as(that)
+            for this, that in zip(
+                self.expressions,
+                other.expressions,
+                strict=True,
+            )
+        )
+
 
 @dataclass
 class Application(_Expression, WithMeta):
@@ -195,6 +306,13 @@ class Application(_Expression, WithMeta):
 
     def pythonic(self) -> tuple:
         return (self.invokable.pythonic(), *self.args.pythonic())
+
+    def is_same_as(self, other: "_SyntaxTree") -> bool:
+        return (
+            isinstance(other, Application)
+            and self.invokable.is_same_as(other.invokable)
+            and self.args.is_same_as(other.args)
+        )
 
 
 class LarkToSyntaxTreeTransformer(Transformer):
